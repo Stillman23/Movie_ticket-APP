48f41e1e20b2572c34fbd78ad57b0c8e1e13ab44
Stillman23 <Scotttillman23@gmail.com>
2018-04-07 07:27:10 -0500

    movie.rb validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ApplicationRecord
+    validates :title, :description, presence: true
+    validates :time, :theatre_room :price, numericallity: {greater_than_or_equal_to: 0.01}
+    validates :title, uniqueness: true
+    has_attached_file :image, styles: {medium: "300x300>" thumb: "100x100>"}
+    validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
