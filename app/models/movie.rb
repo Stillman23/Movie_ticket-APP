fac303b8d9b52aa6a1f00e97c07dcbb1c35e3fa6
Stillman23 <Scotttillman23@gmail.com>
2018-04-07 09:04:52 -0500

    wip

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ApplicationRecord
     validates :title, :description, presence: true
-    validates :time, :theatre_room :price, numericallity: {greater_than_or_equal_to: 0.01}
+    # validates :time, :theatre_room, :price
     validates :title, uniqueness: true
-    has_attached_file :image, styles: {medium: "300x300>" thumb: "100x100>"}
-    validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+    has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+    validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,6 +29,9 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  #devise
+  config.action_mailer.default_url_options= {host: 'localhost', port: 3000}
+
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
@@ -41,7 +44,7 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
 
-  
+
   config.assets.debug = true
 
   # Suppress logger output for asset requests.
